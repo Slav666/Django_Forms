783ebc63b2ebe13e0fe454542d5ac77024dc75e3
Slawomir Dyk <slawomir.dyk@astrosat.net>
2021-08-21 11:12:50 +0100

    Refactored views.py code by using class views

--- reviews/views.py
+++ reviews/views.py
@@ -1,47 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
 from .forms import ReviewForm
 from .models import Review
-from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm
-        return render(request, "reviews/review.html", {"form": form})
-
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-
-# def review(request):
-#     if request.method == "POST":
-#         form = ReviewForm(request.POST)
-
-#         if form.is_valid():
-#             review = Review(
-#                 user_name=form.cleaned_data["user_name"], review_text=form.cleaned_data["review_text"], rating=form.cleaned_data["rating"])
-#             form.save()
-#             return HttpResponseRedirect("/thank-you")
-
-#     else:
-#         form = ReviewForm()
-
-#     return render(request, "reviews/review.html", {"form": form})
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
 
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thankyou.html"
 
-class ReviewsListView(TemplateView):
-    template_name = "reviews/review_list.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+class ReviewsListView(ListView):
+    template_name = "reviews/review_list.html"
+    model = Review
+
